f2a3dd5935d0994bae25d2b43caa50195e518c95
Himanshu Jindal <164390289+Vasu-04@users.noreply.github.com>
2024-09-11 17:39:00 +0530

    Update Problem1_TwoSum.java

--- Problems/Array/Problem1_TwoSum.java
+++ Problems/Array/Problem1_TwoSum.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    int targetIndex1, targetIndex2;
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < (nums.length); i++) {
+            for (int j = i + 1; j < (nums.length); j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    targetIndex1 = i;
+                    targetIndex2 = j;
+                    break;
+                }
+            }
+        }
+        int[] output = new int[2];
+        output[0] = targetIndex1;
+        output[1] = targetIndex2;
+        return output;
+    }
+}
