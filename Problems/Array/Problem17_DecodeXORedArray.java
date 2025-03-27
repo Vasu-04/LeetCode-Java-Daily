7ddc0df6640183c048a0138a9449ab7e6f6785fd
Himanshu Jindal <164390289+Vasu-04@users.noreply.github.com>
2025-03-27 15:52:43 +0530

    Create Problem17_DecodeXORedArray.java

---
+++ Problems/Array/Problem17_DecodeXORedArray.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class Solution {
+    public int[] decode(int[] A, int first) {
+        int n = A.length, res[] = new int[n + 1];
+        res[0] = first;
+        for (int i = 0; i < n; ++i)
+            res[i + 1] = res[i] ^ A[i];
+        return res;
+    }
+}
