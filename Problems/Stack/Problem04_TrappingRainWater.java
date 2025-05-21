5aa0936d4ec5d31f0f186a2d2cf12667d0331a3a
Himanshu Jindal <164390289+Vasu-04@users.noreply.github.com>
2025-05-22 00:00:57 +0530

    Create Problem04_TrappingRainWater.java

---
+++ Problems/Stack/Problem04_TrappingRainWater.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int trap(int[] height) {
+    int n = height.length;
+    int totalWater = 0;
+    Stack<Integer> stack = new Stack<>();
+
+    for (int i = 0; i < n; i++) {
+        while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+            int top = stack.pop();
+            if (stack.isEmpty()) break;
+
+            int distance = i - stack.peek() - 1;
+            int boundedHeight = Math.min(height[i], height[stack.peek()]) - height[top];
+            totalWater += distance * boundedHeight;
+        }
+        stack.push(i);
+    }
+
+    return totalWater;
+}
+
+}
