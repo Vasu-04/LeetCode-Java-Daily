baea1b93c83ea90c119d2165fe06d54619cb7687
Himanshu Jindal <164390289+Vasu-04@users.noreply.github.com>
2025-05-25 15:01:45 +0530

    Create Problem01_ImplementQueueusingStack.java

---
+++ Problems/Queue/Problem01_ImplementQueueusingStack.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+class MyQueue {
+    private Stack<Integer> stack1 = new Stack<>();
+    private Stack<Integer> stack2 = new Stack<>();
+
+    public MyQueue() {
+
+    }
+
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    public int pop() {
+        while(stack1.size()!=1) stack2.push(stack1.pop());
+        int n = stack1.pop();
+        while (!stack2.isEmpty()) stack1.push(stack2.pop());
+        return n;
+    }
+
+    public int peek() {
+        while(stack1.size()!=1) stack2.push(stack1.pop());
+        int n = stack1.peek();
+        while (!stack2.isEmpty()) stack1.push(stack2.pop());
+        return n;
+    }
+
+    public boolean empty() {
+        return stack1.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
